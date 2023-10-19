96d6b2da0dfdaaf49bf979122f2cb8af04fc2d9e
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-10-19 11:44:23 -0700

    added things

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,17 +1,22 @@
 
-
-    public class FizzBuzz {
-
-    int fizz;
-    int buzz;
+public class FizzBuzz {
 
     public void run() {
-    int i = 1;
-    while (i<=100) {
-        System.out.println(i++);
+        int i = 1;
+        while (i<=100) {
+            System.out.println(i++);
+        }
+        if (i == 1) {
+            System.out.println("Fizz");
+        }
+        if (i == 5) {
+            System.out.println("Buzz");
+        }
+        if (i ==15) {
+            System.out.println("FizzBuzz");
+        }
     }
-    }
-   
+
     public static void main(String[] args)
     {
         FizzBuzz hw = new FizzBuzz();
