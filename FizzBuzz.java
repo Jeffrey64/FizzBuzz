e974c8d1086736724bbdfabb1319a8a69ad007c7
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-10-25 11:25:47 -0700

    Commit message

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,17 +1,20 @@
 
 public class FizzBuzz {
-    
+
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++)
-            System.out.println("i++"); 
-            if (i / 15 * 15 == i) {
-            System.out.println("FizzBuzz");
-        
-        } else if (i / 5 * 5 == i) {
-            System.out.println("Buzz");
-        
-        } else if (i / 3 * 3 == i) {
-            System.out.println("Fizz");
-        } 
+        for (int i = 1; i <= 100; i++) { 
+            if ( i / 15 * 15 == i) {
+                System.out.println("FizzBuzz");
+
+            } else if ( i / 5 * 5 == i) {
+                System.out.println("Buzz");
+
+            } else if ( i / 3 * 3 == i) {
+                System.out.println("Fizz");
+                
+            } else { 
+                System.out.println(i);
+            }
         }
     }
+}
