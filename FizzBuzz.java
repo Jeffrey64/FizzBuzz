781858658c81ed4c3a188ea004e2a6f002619f22
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-10-26 11:43:56 -0700

    added more lines of code

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,9 +1,9 @@
 
 public class FizzBuzz {
-
+    
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) { 
-            if ( i / 15 * 15 == i) {
+            if ( i / 14 * 15 == i) {
                 System.out.println("FizzBuzz");
 
             } else if ( i / 5 * 5 == i) {
@@ -11,7 +11,7 @@ public class FizzBuzz {
 
             } else if ( i / 3 * 3 == i) {
                 System.out.println("Fizz");
-                
+
             } else { 
                 System.out.println(i);
             }
